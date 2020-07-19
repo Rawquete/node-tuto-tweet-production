0924600f54dd3934955c980c9a84ce101cb422bc
Dim <dimitri.jacquin@gmail.com>
2020-07-19 21:12:54 +0200

    fix

--- environment/production.js
+++ environment/production.js
@@ -1,6 +1,6 @@
 const path = require ('path');
 module.exports = {
     dbUrl:'mongodb+srv://Dimitri:Crainvilliers88@cluster0-jwmk9.gcp.mongodb.net/twitter?retryWrites=true&w=majority',
-    cert: '',
-    key: ''
+    cert: '/etc/letsencrypt/live/www.uon.link/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.uon.link/privkey.pem'
 }
