b845801abedc6b905ecbc40dd2217c9eb2a5d6a0
Dim <dimitri.jacquin@gmail.com>
2020-07-19 22:37:35 +0200

    Update pm2

--- environment/production.js
+++ environment/production.js
@@ -2,5 +2,7 @@ const path = require ('path');
 module.exports = {
     dbUrl:'mongodb+srv://Dimitri:Crainvilliers88@cluster0-jwmk9.gcp.mongodb.net/twitter?retryWrites=true&w=majority',
     cert: '/etc/letsencrypt/live/www.uon.link/fullchain.pem',
-    key: '/etc/letsencrypt/live/www.uon.link/privkey.pem'
+    key: '/etc/letsencrypt/live/www.uon.link/privkey.pem',
+    portHttp: 80,
+    portHttps: 443
 }
